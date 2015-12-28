1e0f77a61b134c47bf94ed9c33aeb263eade4567
Sergey Parshukov <codedby@bugman.me>
2015-12-28 19:12:18 +0300

    Psychedelic animation

--- webgl/main.js
+++ webgl/main.js
@@ -8,25 +8,59 @@ var camera = new THREE.OrthographicCamera(
     1000
 );
 camera.position.z = 1;
-camera.zoom = 1.5;
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight); false;
+renderer.antialias = true;
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(512, 512, 1);
-var cube = new THREE.Mesh(geometry);
-scene.add(cube);
+var bg = new THREE.Mesh(
+    new THREE.PlaneGeometry(500, 500),
+    new THREE.MeshBasicMaterial({color: 0xffffff})
+);
+scene.add(bg);
 
-var textureInit = function (texture) {
-    cube.material = new THREE.MeshBasicMaterial({map: texture});
+var mask = new THREE.Mesh(
+    new THREE.RingGeometry(220, 400, 50),
+    new THREE.MeshBasicMaterial({color: 0x000000, transparent: true})
+);
+mask.renderOrder = 2;
+scene.add(mask);
+
+var planeGeometry = new THREE.PlaneGeometry(512, 512);
+var tempMaterial = new THREE.MeshBasicMaterial({color: 0x000000, transparent: true})
+
+var origin = new THREE.Mesh(planeGeometry, tempMaterial);
+origin.renderOrder = 0;
+scene.add(origin);
+
+var overlay = new THREE.Mesh(planeGeometry, tempMaterial);
+overlay.renderOrder = 1;
+scene.add(overlay);
+
+// overlay.rotation.z = 0.01;
+overlay.scale.x = 0.96;
+overlay.scale.y = 0.96;
+
+var textureInit = function(texture) {
+    var mat = new THREE.MeshBasicMaterial({map: texture, transparent: true});
+    origin.material = mat;
+    overlay.material = mat;
 };
 
 var loader = new THREE.TextureLoader();
 loader.load('original.png', textureInit);
 
+var frameNumber = 0;
 function render() {
     requestAnimationFrame(render);
+
+    var t = frameNumber / 180 * Math.PI;
+    overlay.position.x = 3 * Math.cos(t);
+    overlay.position.y = 3 * Math.sin(t);
+    overlay.rotation.z = 0.1 * Math.cos(t);
+    frameNumber++;
+
     renderer.render(scene, camera);
 };
 render();
